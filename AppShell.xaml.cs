a31448dffc5e38fd724a6a513db6b0aaf67826a3
Didier Guerrero <didier.guerrero@udla.ede.ec>
2023-12-27 07:33:38 -0500

    Modificacion del AppShell y personalizacion de la aplicacion

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,5 +5,6 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute(nameof(DG_Views.DG_Notas), typeof(DG_Views.DG_Notas));
+    }
 }
